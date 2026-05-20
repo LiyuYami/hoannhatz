30d50c16f4e0fc4923c390198177520d2fe9e3fb
LiyuYami <yennhithichdu@gmail.com>
2026-05-20 19:02:40 +0700

    Add files via upload

---
+++ YNEZScript.lua
@@ -0,0 +1,6 @@
+getgenv().ScriptTitle = "Yến Nhi Roblox"
+getgenv().ScriptSubTitle = "Tổng Hợp No Key [ VIP ✅ ]"
+getgenv().ScriptImage = "https://i.ibb.co/WvWvcwFH/IMG-20260320-135614.jpg"
+getgenv().ScriptAuthorName = "Yến Nhi Roblox"
+getgenv().ScriptAuthorSubTitle = "https://discord.gg/AWaBxVXxB"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LiyuYami/hoannhatz/refs/heads/main/EZScript.lua"))()
